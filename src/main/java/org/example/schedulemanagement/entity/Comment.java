0d06eba263a661a151debf55459b03ff2ce1a350
d-perado <sjm01065@naver.com>
2025-11-03 20:48:10 +0900

    TimeAt 제거 불필요한 관계가 늘어난다고 판단

--- src/main/java/org/example/schedulemanagement/entity/Comment.java
+++ src/main/java/org/example/schedulemanagement/entity/Comment.java
@@ -3,16 +3,33 @@ package org.example.schedulemanagement.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Entity
 @Table(name = "comments")
+@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor
-public class Comment extends TimeAt{
+public class Comment{
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "COMMENT_ID")
     private Long id;
     private String comment;
     private String writer;
-    private Long schedule;
+    @Column(name = "SCHEDULE_ID")
+    private Long scheduleId;
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    public Comment(String comment, String writer, Long scheduleId){
+        this.comment = comment;
+        this.writer = writer;
+        this.scheduleId =scheduleId;
+    }
 }
--- src/main/java/org/example/schedulemanagement/entity/Schedule.java
+++ src/main/java/org/example/schedulemanagement/entity/Schedule.java
@@ -4,14 +4,23 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Entity
 @Table(name = "schedules")
+@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor
-public class Schedule extends TimeAt{
+public class Schedule{
 
     @Id
+    @Column(name = "SCHEDULE_ID")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long scheduleId;
     @Column(nullable = false, length = 50)
@@ -22,11 +31,17 @@ public class Schedule extends TimeAt{
     private String writer;
     @Column(nullable = false, length = 50)
     private String password;
+    @OneToMany
+    @JoinColumn(name = "SCHEDULE_ID")
+    private List<Comment> comments = new ArrayList<>();
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 
 
     @Builder
     public Schedule(String title, String content, String writer, String password) {
-        super();
         this.title = title;
         this.content = content;
         this.writer = writer;
