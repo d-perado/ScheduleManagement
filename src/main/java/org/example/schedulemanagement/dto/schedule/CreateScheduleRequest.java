a2edbacfd351ce4e093b5f62497f42a48e03c165
d-perado <sjm01065@naver.com>
2025-11-04 20:18:10 +0900

    사용하지 않는 클래스, import 제거

--- src/main/java/org/example/schedulemanagement/dto/schedule/CreateScheduleRequest.java
+++ src/main/java/org/example/schedulemanagement/dto/schedule/CreateScheduleRequest.java
@@ -1,7 +1,6 @@
 package org.example.schedulemanagement.dto.schedule;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
