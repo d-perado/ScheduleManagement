1dbc118af45c4f6f4034bdf1ed7c2726644af8a1
d-perado <sjm01065@naver.com>
2025-11-06 09:12:14 +0900

    의미없는 엔터 제거

--- src/main/java/org/example/schedulemanagement/dto/comment/CreateCommentRequest.java
+++ src/main/java/org/example/schedulemanagement/dto/comment/CreateCommentRequest.java
@@ -11,10 +11,8 @@ public class CreateCommentRequest {
     @NotBlank(message = "작성자는 비어있을 수 없습니다.")
     @Size(max = 100, message = "작성자의 이름은 100자 이내여야 합니다.")
     private String writer;
-
     @NotBlank(message = "패스워드는 비어있을 수 없습니다.")
     private String password;
-
     @NotBlank(message = "댓글 내용은 비어있을 수 없습니다.")
     private String comment;
 }
